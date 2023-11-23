852ac8824d75cba9471481848befca1638638a4f
Krzysztof <krzysztof.wroblewski@uzh.ch>
2023-11-23 22:44:06 +0100

    Fix Decay Function

--- src/stock_ranker.py
+++ src/stock_ranker.py
@@ -40,14 +40,18 @@ class StockRanker:
         return score_series
 
     def apply_decay(self):
-        self.stocks_df['days_from_recent'] = (self.stocks_df['date'].max() - self.stocks_df['date']).dt.days
+        recent_date = self.stocks_df['date'].max()
+        self.stocks_df['months_from_recent'] = (
+                (recent_date.year - self.stocks_df['date'].dt.year) * 12
+                + recent_date.month - self.stocks_df['date'].dt.month
+        )
         if self.decay_function == 'linear':
-            self.stocks_df['decay'] = 1 - (self.decay_rate / 100) * self.stocks_df['days_from_recent']
+            self.stocks_df['decay'] = 1 - (self.decay_rate / 100) * self.stocks_df['months_from_recent']
         elif self.decay_function == 'exponential':
-            self.stocks_df['decay'] = np.exp(-self.decay_rate * self.stocks_df['days_from_recent'])
+            self.stocks_df['decay'] = np.exp(-self.decay_rate * self.stocks_df['months_from_recent'])
         elif self.decay_function == 'logarithmic':
-            # Avoid log(0) issue
-            self.stocks_df['decay'] = 1 / np.log(self.stocks_df['days_from_recent'] + 1)
+            # Avoid log(0)
+            self.stocks_df['decay'] = 1 / (1 + self.decay_rate * np.log(1 + self.stocks_df['months_from_recent']))
         else:
             # No decay
             self.stocks_df['decay'] = 1
