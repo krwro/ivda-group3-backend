74d39baa15e7d347ce98d859e03662254d6db017
Krzysztof <krzysztof.wroblewski@uzh.ch>
2023-12-05 10:21:25 +0100

    Add More Features to Model

--- src/model.py
+++ src/model.py
@@ -8,20 +8,58 @@ from pydantic import BaseModel
 class Stock(BaseModel):
     _id: str
     date: datetime
-    price: float
     symbol: str
-    revenue: Optional[float] = None
-    netIncome: Optional[float] = None
-    eps: Optional[float] = None
-    roe: Optional[float] = None
-    roic: Optional[float] = None
-    F0_researchAndDevelopmentExpenses: Optional[float] = None
-    F1_researchAndDevelopmentExpenses: Optional[float] = None
-    F2_researchAndDevelopmentExpenses: Optional[float] = None
+
+    price: Optional[float] = None
     F1_price: Optional[float] = None
     F2_price: Optional[float] = None
+
+    grossProfitMargin: Optional[float] = None
+    eps: Optional[float] = None
+    dividendYield: Optional[float] = None
+    grahamNumber: Optional[float] = None
+    cashFlowToDebtRatio: Optional[float] = None
+    operatingCashFlowPerShare: Optional[float] = None
+    returnOnAssets: Optional[float] = None
+    roe: Optional[float] = None
+    debtEquityRatio: Optional[float] = None
+
+    revenue: Optional[float] = None
+    F1_revenue: Optional[float] = None
+    F2_revenue: Optional[float] = None
+
+    netIncome: Optional[float] = None
     F1_netIncome: Optional[float] = None
     F2_netIncome: Optional[float] = None
+
+    grossProfit: Optional[float] = None
+    F1_grossProfit: Optional[float] = None
+    F2_grossProfit: Optional[float] = None
+
+    interestCoverage: Optional[float] = None
+    F1_interestCoverage: Optional[float] = None
+    F2_interestCoverage: Optional[float] = None
+
+    operatingIncome: Optional[float] = None
+    F1_operatingIncome: Optional[float] = None
+    F2_operatingIncome: Optional[float] = None
+
+    bookValuePerShare: Optional[float] = None
+    F1_bookValuePerShare: Optional[float] = None
+    F2_bookValuePerShare: Optional[float] = None
+
+    tangibleAssetValue: Optional[float] = None
+    F1_tangibleAssetValue: Optional[float] = None
+    F2_tangibleAssetValue: Optional[float] = None
+
+    workingCapital: Optional[float] = None
+    F1_workingCapital: Optional[float] = None
+    F2_workingCapital: Optional[float] = None
+
+    priceToSalesRatio: Optional[float] = None
+    F1_priceToSalesRatio: Optional[float] = None
+    F2_priceToSalesRatio: Optional[float] = None
+
 
     def to_json(self):
         return jsonable_encoder(self, exclude_none=True)
